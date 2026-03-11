04ee41fba4649a26a1565485b086377fcf0e4c1f
dibaos2007-ship-it <dibaos2007@gmail.com>
2026-03-11 16:01:54 -0500

    Implementar el método para contar el número de caracteres en una cadena

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -438,13 +438,26 @@ Rock crushes Scissors
     }
 
     public double areaCirculo(double radio) {
-        return 0.0;
-    }
+return Math.PI * Math.pow(radio, 2);
+}
 
     public String zoodiac(int day, int month) {
-        return "";
-    }
-
-
+    if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) return "Aries";
+    if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) return "Tauro";
+    if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) return "Geminís";
+    if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) return "Cáncer";
+    if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) return "Leo";
+    if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) return "Virgo";
+    if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) return "Libra";
+    if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) return "Escorpio";
+    if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) return "Sagitario";
+    if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) return "Capricornio";
+    if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) return "Acuario";
+    return "Aries";
 }
+
+ }
+
+
+
 
